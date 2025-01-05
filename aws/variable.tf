3c9602cec00e9a7b92c93917f75ec54c5b37f1d7
eoinbyrnefj <eoin.byrne@fujitsu.com>
2025-01-05 00:30:06 +0000

    Create variable.tf

---
+++ aws/variable.tf
@@ -0,0 +1,35 @@
+variable "ami" {
+  type        = string
+  description = "Ubuntu AMI ID in eu-west-1 Region"
+  default     = "ami-04bd4a6a67aa8e86e"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type"
+  default     = "t2.micro"
+}
+
+variable "name_tag" {
+  type        = string
+  description = "Name of the EC2 instance"
+  default     = "Eoin's EC2 Instance"
+}
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
